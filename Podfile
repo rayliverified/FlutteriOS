97d0dd85a4ca969df3b3d1aefdbabac8375c4023
Ray <ray.li.verified@gmail.com>
2019-05-17 17:14:15 -0500

    Initialize Flutter Module

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'flutterios' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FlutteriOS
+
+end
+
+flutter_application_path = 'my_flutter/'
+eval(File.read(File.join(flutter_application_path, '.ios', 'Flutter', 'podhelper.rb')), binding)
--- flutterios/ViewController.swift
+++ flutterios/ViewController.swift
@@ -1,20 +1,33 @@
 //
 //  ViewController.swift
-//  flutterios
+//  FlutterDemo
 //
-//  Created by Christopher Lam on 5/17/19.
-//  Copyright © 2019 Sample. All rights reserved.
+//  Created by Christopher Lam on 5/14/19.
+//  Copyright © 2019 nocd. All rights reserved.
 //
 
 import UIKit
+import Flutter
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.green
+        let button = UIButton(type:UIButton.ButtonType.custom)
+        button.addTarget(self, action: #selector(handleButtonAction), for: .touchUpInside)
+        button.setTitle("Press me", for: UIControl.State.normal)
+        button.frame = CGRect(x: 80.0, y: 210.0, width: 160.0, height: 40.0)
+        button.backgroundColor = UIColor.blue
+        self.view.addSubview(button)
     }
+    
+    @objc func handleButtonAction() {
+        let flutterViewController = FlutterViewController(nibName: nil, bundle: nil);
+        flutterViewController.isViewOpaque = false
+        flutterViewController.modalPresentationStyle = UIModalPresentationStyle.overCurrentContext
 
-
+        let flutterEngine = (UIApplication.shared.delegate as? AppDelegate)?.flutterEngine;
+        flutterEngine?.run(withEntrypoint: nil);
+        self.present(flutterViewController, animated: false, completion: nil)
+    }
 }
-
