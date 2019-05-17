6f9521fa999165f4a6c2815950506a47801f9e88
Ray <ray.li.verified@gmail.com>
2019-05-17 15:47:56 -0500

    Initialize Empty iOS Project

---
+++ flutterios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flutterios
+//
+//  Created by Christopher Lam on 5/17/19.
+//  Copyright © 2019 Sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
