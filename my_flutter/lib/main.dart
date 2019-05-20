2084e057d78b5f238c4b971eeff5ebbc05e16e67
Ray <ray.li.verified@gmail.com>
2019-05-20 09:54:22 -0500

    Import Flutter Project
    *Import Flutter project.

--- my_flutter/lib/main.dart
+++ my_flutter/lib/main.dart
@@ -1,110 +1,183 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_android/bloc/BlocProvider.dart';
+import 'package:flutter_android/page/page_main.dart';
+import 'package:flutter_android/themes.dart';
+import 'package:flutter_android/utils.dart';
+import 'package:rxdart/rxdart.dart';
+
+void main() => runApp(BlocProvider<AppBloc>(bloc: AppBloc(), child: App()));
+
+class AppBloc implements BlocBase {
+  static const channel = MethodChannel('app');
+
+  String _page = window.defaultRouteName ?? "";
+  bool _back = false;
+
+  BehaviorSubject<String> pageController = BehaviorSubject<String>();
+  ValueObservable get getPage => pageController;
+
+  void dispose() {
+    pageController.close();
+  }
+
+  AppBloc() {
+    initPlatformChannels();
+  }
+
+  void updatePage(String page) {
+    _page = page;
+    pageController.sink.add(_page);
+  }
+
+  void updateBack(bool back) {
+    if (_back != back) {
+      _back = back;
+      sendBackStatus(_back);
+    }
+  }
+
+  void initPlatformChannels() async {
+    // Receive method invocations from platform and return results.
+    channel.setMethodCallHandler((MethodCall call) async {
+      switch (call.method) {
+        case 'page':
+          updatePage(call.arguments);
+          return;
+        case 'navigation':
+          switch (call.arguments) {
+            case "back":
+              {}
+          }
+          return;
+        default:
+          throw MissingPluginException();
+      }
+    });
+  }
+
+  void navigate(Navigation navigation) {
+    switch (navigation) {
+      case Navigation.BACK:
+        navigateBack();
+        return;
+    }
+  }
+
+  void navigateBack() async {
+    try {
+      print(await channel.invokeMethod("navigation", "back"));
+    } on PlatformException catch (e) {
+      print('Failed: ${e.message}');
+    } on MissingPluginException {
+      print('Not implemented');
+    }
+  }
+
+  void sendBackStatus(bool backStatus) async {
+    try {
+      print(await channel.invokeMethod("back_status", backStatus));
+    } on PlatformException catch (e) {
+      print('Failed: ${e.message}');
+    } on MissingPluginException {
+      print('Not implemented');
+    }
+  }
+}
+
+enum Navigation { BACK }
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final AppBloc appBloc = BlocProvider.of<AppBloc>(context);
+    return StreamBuilder(
+        stream: appBloc.getPage,
+        initialData: appBloc._page,
+        builder: (context, snapshot) {
+          return _widgetForRoute(snapshot.data);
+        });
+  }
+}
+
+Widget _widgetForRoute(String route) {
+  print("Switch Route: " + route);
+  switch (route) {
+    case 'page_main':
+      return MyApp();
+    case 'page_transparent':
+      return Transparent();
+    default:
+      return Center(
+        child: Text(
+          'Unknown route: $route',
+          textDirection: TextDirection.ltr,
+        ),
+      );
+  }
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    final AppBloc appBloc = BlocProvider.of<AppBloc>(context);
+    return new MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in a Flutter IDE). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: defaultTheme,
+      home: new MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class Transparent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    final AppBloc appBloc = BlocProvider.of<AppBloc>(context);
+
+    return new MaterialApp(
+        title: 'Flutter Transparent',
+        theme: transparentTheme,
+        home: Scaffold(
+          body: Center(
+              child: GestureDetector(
+                  onTap: () {
+                    appBloc.navigate(Navigation.BACK);
+                  },
+                  child: Text('Transparent Scaffold Background'))),
+          backgroundColor: HexColor('#00FFFFFF'),
+        ));
   }
 }
+
+//UNUSED: App Provider implementation.
+//class AppBloc implements BlocBase {
+//  AppProvider appProvider = AppProvider();
+//
+//  //UNUSED: Example using dart:async Streams.
+//  StreamController<String> appController = StreamController();
+//  Stream get getPage => appController.stream;
+//
+//  AppBloc() {
+//  }
+//
+//  void updatePage(String page) {
+//    appProvider.updatePage(page);
+//    appController.sink.add(appProvider._page);
+//  }
+//
+//  void dispose() {
+//    appController.close();
+//  }
+//}
+
+//UNUSED: App Provider wrapper for complex value mutations.
+//class AppProvider {
+//  String _page = window.defaultRouteName ?? "";
+//
+//  void updatePage(String page) {
+//    _page = page;
+//  }
+//}
