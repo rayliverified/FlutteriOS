d14a1144c1898b3104a513e18073e53676c7810b
Ray <ray.li.verified@gmail.com>
2019-05-21 16:25:59 -0500

    Update Method Channels

--- flutterios/ViewController.swift
+++ flutterios/ViewController.swift
@@ -11,6 +11,12 @@ import Flutter
 
 class ViewController: UIViewController {
     
+    let CHANNEL_METHOD_PAGE = "CHANNEL_METHOD_PAGE";
+    let CHANNEL_METHOD_NAVIGATION = "CHANNEL_METHOD_NAVIGATION";
+    let NAVIGATION_BACK = "NAVIGATION_BACK";
+    let NAVIGATION_CLOSE = "NAVIGATION_CLOSE";
+    let CHANNEL_METHOD_BACK_STATUS = "CHANNEL_METHOD_BACK_STATUS";
+    
     var flutterView: FlutterViewController
     var flutterChannel: FlutterMethodChannel
     
@@ -35,29 +41,30 @@ class ViewController: UIViewController {
         button.frame = CGRect(x: 80.0, y: 210.0, width: 160.0, height: 40.0)
         button.backgroundColor = UIColor.blue
         self.view.addSubview(button)
-        let button2 = UIButton(type:UIButton.ButtonType.custom)
-        button2.addTarget(self, action: #selector(handleButtonAction2), for: .touchUpInside)
-        button2.setTitle("Page Main", for: UIControl.State.normal)
-        button2.frame = CGRect(x: 80.0, y: 260.0, width: 160.0, height: 40.0)
-        button2.backgroundColor = UIColor.red
-        self.view.addSubview(button2)
-        let button3 = UIButton(type:UIButton.ButtonType.custom)
-        button3.addTarget(self, action: #selector(handleButtonAction2), for: .touchUpInside)
-        button3.setTitle("Page Transparent", for: UIControl.State.normal)
-        button3.frame = CGRect(x: 80.0, y: 310.0, width: 160.0, height: 40.0)
-        button3.backgroundColor = UIColor.red
-        self.view.addSubview(button3)
+//        let button2 = UIButton(type:UIButton.ButtonType.custom)
+//        button2.addTarget(self, action: #selector(handleButtonAction2), for: .touchUpInside)
+//        button2.setTitle("Page Main", for: UIControl.State.normal)
+//        button2.frame = CGRect(x: 80.0, y: 260.0, width: 160.0, height: 40.0)
+//        button2.backgroundColor = UIColor.red
+//        self.view.addSubview(button2)
+//        let button3 = UIButton(type:UIButton.ButtonType.custom)
+//        button3.addTarget(self, action: #selector(handleButtonAction3), for: .touchUpInside)
+//        button3.setTitle("Page Transparent", for: UIControl.State.normal)
+//        button3.frame = CGRect(x: 80.0, y: 310.0, width: 160.0, height: 40.0)
+//        button3.backgroundColor = UIColor.red
+//        self.view.addSubview(button3)
         
-        flutterView.setInitialRoute("page_main")
+        flutterView.setInitialRoute("PAGE_DELETE_ACCOUNT")
         flutterView.isViewOpaque = false
         flutterView.modalPresentationStyle = UIModalPresentationStyle.overCurrentContext
         
         flutterChannel.setMethodCallHandler {
             (call: FlutterMethodCall, result: FlutterResult) -> Void in
             switch (call.method) {
-            case "navigation":
+            case self.CHANNEL_METHOD_NAVIGATION:
                 switch(call.arguments as! String) {
-                case "back":
+                case self.NAVIGATION_CLOSE:
+                    print("NAVIGATION_CLOSE")
                     self.flutterView.dismiss(animated: true, completion: nil)
                 default:
                     return
@@ -74,10 +81,21 @@ class ViewController: UIViewController {
     
     @objc func handleButtonAction() {
         self.present(flutterView, animated: false, completion: nil)
+        flutterChannel.invokeMethod(CHANNEL_METHOD_PAGE, arguments: "PAGE_DELETE_ACCOUNT") {
+            (result: Any?) -> Void in
+            if let error = result as? FlutterError {
+                print("%@ failed: %@" + error.message!)
+            } else if FlutterMethodNotImplemented.isEqual(result) {
+                print("%@ not implemented")
+            } else {
+                print("%@")
+            }
+        }
     }
     
     @objc func handleButtonAction2() {
-        flutterChannel.invokeMethod("page", arguments: "page_main") {
+        self.present(flutterView, animated: false, completion: nil)
+        flutterChannel.invokeMethod(CHANNEL_METHOD_PAGE, arguments: "PAGE_DELETE_ACCOUNT") {
             (result: Any?) -> Void in
             if let error = result as? FlutterError {
                 print("%@ failed: %@" + error.message!)
@@ -90,7 +108,7 @@ class ViewController: UIViewController {
     }
     
     @objc func handleButtonAction3() {
-        flutterChannel.invokeMethod("page", arguments: "page_transparent") {
+        flutterChannel.invokeMethod(CHANNEL_METHOD_PAGE, arguments: "PAGE_TRANSPARENT") {
             (result: Any?) -> Void in
             if let error = result as? FlutterError {
                 print("%@ failed: %@" + error.message!)
